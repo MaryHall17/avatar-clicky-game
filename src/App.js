08140a4e918d976967b056615ee4cf4a3982f130
Mary Lorraine Hall <mh6uj@virginia.edu>
2018-04-01 17:06:39 -0400

    scoring function works

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Title from "./component/Title";
 import './App.css';
 
 class App extends Component {
-	state = {
-		card,
+    state = {
+        card,
         click:{1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0},
-        score: 0
+        score: 0,
+        highScore: 0
      };
 
    shuffleArray = card => {
@@ -26,13 +27,6 @@ class App extends Component {
 
 
     tallyScore = id => { 
-       // console.log("state " + this.state.click[id]);
-       // var cardClickHistory=this.state.click;
-       // console.log( "pre-history " + cardClickHistory);
-        //cardClickHistory[id]+=1;
-       // console.log("after " + cardClickHistory);
-       // this.setState({ click:cardClickHistory});
-       // console.log("history set " + this.state.click[id]);
         if(this.state.click[id]===0) {
             console.log("original score: " + this.state.score);
            var cardClickHistory=this.state.click;
@@ -45,29 +39,38 @@ class App extends Component {
            this.setState({ click: cardClickHistory });
            this.setState({ score: tally });
            console.log("history set " + this.state.click[id]);
-          } //else {
-           //this.setState({ score: this.state.score=0});
-            //this.setState({ click: this.state.click=0});
-          //}
-        };
+          } else if(this.state.click[id]===1 && this.state.score > this.state.highScore) {
+            var reset=0;
+            this.setState({ score: reset});
+            this.setState({ highScore: this.state.score});
+            this.setState({ click: {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0} });
+            console.log("score " + this.state.score);
+            console.log("high score " + this.state.highScore);
+          } else {
+          reset=0;
+          this.setState({ score: reset});
+          this.setState({ click: {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0} });
+          console.log(this.state.score);
+         }
+       };
 
-	render () {
-		return (
-			<Wrapper>
-			<Title>Click Game</Title>
-			{this.state.card.map(card =>
-				<ClickCard
-                shuffleCard={this.shuffleArray}
+    render () {
+        return (
+            <Wrapper>
+            <Title>Click Game</Title>
+            {this.state.card.map(card =>
+                <ClickCard
+                //shuffleCard={this.shuffleArray}
                 tallyScore={this.tallyScore}
-				id={card.id}
-				image={card.image}
+                id={card.id}
+                image={card.image}
                 key={card.id}
-				name={card.name}
-				/>
-			)}
-			</Wrapper>
-		);
-	}
+                name={card.name}
+                />
+            )}
+            </Wrapper>
+        );
+    }
 }
 
 
